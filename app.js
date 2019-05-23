92d37ad5d3f68b4e8677c3ce168623c45dbe1d16
Ali Unlu <33809587+aliunlu97@users.noreply.github.com>
2019-05-23 19:46:06 +0100

    checkLogin for every page done. No unaddressed error.

--- app.js
+++ app.js
@@ -36,38 +36,140 @@ app.use(session({secret: process.env.SECRET_KEY}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Serving static files in the public folder
+app.use(express.static(__dirname + '/public'));
+
+
+function checkLogin(req, res, next){
+   if(req.session.user){
+     next();     //If session exists, proceed to page
+   }
+   else{
+     var err = new Error("Not logged in!");
+     next(err);  //Error, trying to access unauthorized page!
+   }
+}
+
+function loginRedirect(err, req, res, next) {
+  console.log(err);
+  res.redirect('/login');
+}
+
+function continueWithNoLogin(err, req, res, next) {
+  console.log(err);
+
+  if (req.method === "GET") {
+    console.log("Rendering");
+    if (req.originalUrl==='/login') {
+      res.render('login.ejs',{
+        logged:"false",
+      });
+    }
+    else if(req.originalUrl==='/signup') {
+      res.render('signup.ejs',{
+        logged:"false",
+        message: ""
+      });
+    }
+    else if (req.originalUrl==='/privacy') {
+      res.render('privacy.ejs',{
+        logged:"false",
+        lastModified: {string: "21/05/2019", datetime: "2019-05-21",},
+      });
+    }
+    else if (req.originalUrl==='/tos') {
+      res.render('tos.ejs',{
+        logged:"false",
+        lastModified: {string: "21/05/2019", datetime: "2019-05-21",},
+      });
+    }
+    else if (req.originalUrl==='/') {
+      res.render('index.ejs',{
+        logged:"false",
+      });
+    }
+    // Bad requests with GET
+    else {
+      res.status(404);
+      res.render('error.ejs',{
+        logged:"false",
+        errorMessage: "404 Not found"
+      });
+    }
+  }
+  else if(req.method === "POST"){
+    if (req.originalUrl==='/signup' || req.originalUrl==='/login') {
+      console.log("Proceeding");
+      next();
+    }
+    // Bad requests with POST
+    else{
+      res.status(404);
+      res.render('error.ejs',{
+        logged:"false",
+        errorMessage: "404 Not found"
+      });
+    }
+  }
+  // Bad requests with DELETE, PUT, etc.
+  else {
+    res.status(404);
+    res.render('error.ejs',{
+      logged:"false",
+      errorMessage: "404 Not found"
+    });
+  }
+}
+
+
 // Catch all requests that comply with the todos API routes
-require('./server/routes')(app);
+require('./server/routes')(app, checkLogin, loginRedirect, continueWithNoLogin);
 
 
-app.use('/logout', (req, res) => {
+app.get('/logout', checkLogin, (req, res) => {
    req.session.destroy(() => {
       console.log("User logged out.")
    });
    res.redirect('/login');
 });
+app.use('/logout', checkLogin, loginRedirect);
 
-app.use(express.static(__dirname + '/public'));
 
-
-app.get('/privacy', (req, res) => {
-  res.render('privacy.ejs',{lastModified: {string: "21/05/2019", datetime: "2019-05-21",},});
+app.get('/privacy', checkLogin, (req, res) => {
+  res.render('privacy.ejs',{
+    logged:"true",
+    lastModified: {string: "21/05/2019", datetime: "2019-05-21",},
+  });
 });
+app.use('/privacy', checkLogin, continueWithNoLogin);
+
 
 //document.lastModified outputs the last time the html document is rendered
 //via EJS, thus, outputting the last date of access.
-app.get('/tos', (req, res) => {
-  res.render('tos.ejs',{lastModified: {string: "21/05/2019", datetime: "2019-05-21",},});
+app.get('/tos', checkLogin, (req, res) => {
+  res.render('tos.ejs',{
+    logged:"true",
+    lastModified: {string: "21/05/2019", datetime: "2019-05-21",},
+  });
 });
+app.use('/tos', checkLogin, continueWithNoLogin);
 
-app.get('/', (req, res) => {
-  res.render('index.ejs',{});
+
+app.get('/', checkLogin, (req, res) => {
+  res.render('index.ejs',{logged:"true",});
 });
+app.use('/', checkLogin, continueWithNoLogin);
 
-// Setup a default catch-all route that sends back a welcome message in JSON format.
-app.get('*', (req, res) => {
+
+// Setup a default catch-all route that displays the 404 Not Found page.
+app.use('*', checkLogin, (req, res) => {
   res.status(404);
-  res.render('error.ejs',{errorMessage: "404 Not found"});
+  res.render('error.ejs',{
+    logged:"true",
+    errorMessage: "404 Not found"
+  });
 });
+app.use('*', checkLogin, continueWithNoLogin);
+
 
 module.exports = app;
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,45 +2,11 @@ const usersController = require('../controllers').users;
 const postsController = require('../controllers').posts;
 
 
-module.exports = (app) => {
+module.exports = (app, checkLogin, loginRedirect, continueWithNoLogin) => {
   // app.get('/api', (req, res) => res.status(200).send({
   //   message: 'Welcome to the Todos API!',
   // }));
 
-
-  function checkLogin(req, res, next){
-     if(req.session.user){
-       next();     //If session exists, proceed to page
-     }
-     else{
-       var err = new Error("Not logged in!");
-       next(err);  //Error, trying to access unauthorized page!
-     }
-  }
-
-  function loginRedirect(err, req, res, next) {
-    console.log(err);
-    res.redirect('/login');
-  }
-
-  function continueWithNoLogin(err, req, res, next) {
-    console.log(err);
-    console.log(req.originalUrl);
-
-    if (req.method === "GET") {
-      console.log("Rendering");
-      if (req.originalUrl==='/login') {
-        res.render('login.ejs',{});
-      }
-      else if(req.originalUrl==='/signup') {
-        res.render('signup.ejs',{message: ""});
-      }
-    }
-    else{
-      console.log("Proceeding");
-      next();
-    }
-  }
 
   app.use('/signup', checkLogin, (req, res) => {
     res.redirect('/dashboard');
@@ -65,6 +31,7 @@ module.exports = (app) => {
 
   app.get('/dashboard', checkLogin, (req, res) => {
     res.render('dashboard.ejs',{
+      logged:"true",
       name: req.session.user.name,
       affiliation: req.session.user.affiliation,
       position: req.session.user.position,
@@ -82,6 +49,7 @@ module.exports = (app) => {
 
   app.get('/corpus', checkLogin, usersController.list, (req, res, next) => {
     res.render('corpus.ejs',{
+      logged:"true",
       name: req.session.user.name,
       affiliation: req.session.user.affiliation,
       position: req.session.user.position,
@@ -94,6 +62,7 @@ module.exports = (app) => {
 
   app.get('/profile', checkLogin, (req, res) => {
     res.render('profile.ejs',{
+      logged:"true",
       name: req.session.user.name,
       affiliation: req.session.user.affiliation,
       position: req.session.user.position,
@@ -106,7 +75,7 @@ module.exports = (app) => {
 
 
 
-  app.use('/home', checkLogin, loginRedirect);
+  // app.use('/home', checkLogin, loginRedirect);
 
   // app.get('/api/users', usersController.list);
   // app.get('/api/users/:userId',usersController.retrieve);
